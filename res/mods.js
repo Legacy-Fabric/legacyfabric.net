3459cad2213b423da071b05a2014bd825de3f7c1
Luke Schwager <luke@blucobalt.dev>
2022-04-17 13:17:07 -0700

    major bag

---
+++ res/mods.js
@@ -0,0 +1,264 @@
+const mods = {
+    "1.8.9": [
+        {
+            "name": "Alternate Current",
+            "link": "https://modrinth.com/mod/r0v8vy1s",
+            "working": false
+        },
+        {
+            "name": "AnTitanic",
+            "link": "https://modrinth.com/mod/GDpQeUxh",
+            "working": false
+        },
+        {
+            "name": "AutoGG",
+            "link": "https://modrinth.com/mod/autogg",
+            "working": true
+        },
+        {
+            "name": "ChromaHudFabric",
+            "link": "https://modrinth.com/mod/PzuxEnMj",
+            "working": false
+        },
+        {
+            "name": "Don't Clear Chat History",
+            "link": "https://modrinth.com/mod/sUbMm93i",
+            "working": false
+        },
+        {
+            "name": "Durability Speed (Legacy Backport)",
+            "link": "https://modrinth.com/mod/6852vWRO",
+            "working": false
+        },
+        {
+            "name": "Ears",
+            "link": "https://modrinth.com/mod/mfzaZK3Z",
+            "working": false
+        },
+        {
+            "name": "Fatal Blow - Legacy Backport",
+            "link": "https://modrinth.com/mod/BOhW0XDq",
+            "working": false
+        },
+        {
+            "name": "FPS - Display",
+            "link": "https://modrinth.com/mod/DIlqwRFH",
+            "working": false
+        },
+        {
+            "name": "F3ServerInfo",
+            "link": "https://www.curseforge.com/minecraft/mc-mods/f3serverinfo",
+            "working": true
+        },
+        {
+            "name": "Infinite Water Bucket",
+            "link": "https://modrinth.com/mod/nNMXZUPi",
+            "working": true
+        },
+        {
+            "name": "Legacy Elytras",
+            "link": "https://github.com/pixfumy/Legacy-Elytras/releases",
+            "working": true
+        },
+        {
+            "name": "Legacy Extended Hotbar",
+            "link": "https://modrinth.com/mod/twG4qbSN",
+            "working": false
+        },
+        {
+            "name": "Legacy Mod Menu",
+            "link": "https://modrinth.com/mod/XzTYkVLx",
+            "working": false
+        },
+        {
+            "name": "Legacy VanillaFix",
+            "link": "https://modrinth.com/mod/kzsy7on5",
+            "working": true
+        },
+        {
+            "name": "No Enchant Cap (Legacy Backport)",
+            "link": "https://modrinth.com/mod/CcwKyFAw",
+            "working": false
+        },
+        {
+            "name": "Old Block Hit",
+            "link": "https://github.com/TAKfsg/oldblockhit-legacy-fabric/releases",
+            "working": true
+        },
+        {
+            "name": "One Block At A Time",
+            "link": "https://modrinth.com/mod/onebloc",
+            "working": true
+        },
+        {
+            "name": "Optifabric",
+            "link": "https://github.com/RedLime/OptiFabric-Pre1.9/releases",
+            "working": true
+        },
+        {
+            "name": "Punch 2 Prime",
+            "link": "https://modrinth.com/mod/CT5U6tG4",
+            "working": false
+        },
+        {
+            "name": "Subtitles backport",
+            "link": "https://github.com/YetAnotherMcModder/Subtitles-Backport/releases",
+            "working": true
+        },
+        {
+            "name": "ToggleSprint",
+            "link": "https://modrinth.com/mod/togglesprint",
+            "working": true
+        },
+        {
+            "name": "Where-Is-My-Stuff",
+            "link": "https://modrinth.com/mod/JV1NYdVY",
+            "working": false
+        }
+    ],
+    "1.13.2": [
+        {
+            "name": "Alternate Current",
+            "link": "https://modrinth.com/mod/r0v8vy1s",
+            "working": false
+        },
+        {
+            "name": "Infinte Water Bucket",
+            "link": "https://modrinth.com/mod/nNMXZUPi",
+            "working": true
+        },
+        {
+            "name": "No More Bleeding Sheep",
+            "link": "https://modrinth.com/mod/HXXsQvqV",
+            "working": false
+        },
+        {
+            "name": "No Recipe Book (Fabric)",
+            "link": "https://modrinth.com/mod/TvL1V8O5",
+            "working": false
+        },
+        {
+            "name": "Redstone Multimeter Fabric",
+            "link": "https://modrinth.com/mod/X6YTb3G1",
+            "working": false
+        }
+    ],
+    "1.12.2": [
+        {
+            "name": "Alternate Current",
+            "link": "https://modrinth.com/mod/r0v8vy1s",
+            "working": false
+        },
+        {
+            "name": "Don't Clear Chat History",
+            "link": "https://modrinth.com/mod/sUbMm93i",
+            "working": false
+        },
+        {
+            "name": "Durability Speed (Legacy Backport)",
+            "link": "https://modrinth.com/mod/6852vWRO",
+            "working": false
+        },
+        {
+            "name": "Fatal Blow - Legacy Backport",
+            "link": "https://modrinth.com/mod/BOhW0XD",
+            "working": false
+        },
+        {
+            "name": "Infinite Water Bucket",
+            "link": "https://modrinth.com/mod/nNMXZUPi",
+            "working": true
+        },
+        {
+            "name": "Inventory Pause (Cursed)",
+            "link": "https://modrinth.com/mod/pBbOtJk2",
+            "working": false
+        },
+        {
+            "name": "No More Bleeding Sheep",
+            "link": "https://modrinth.com/mod/HXXsQvqV",
+            "working": false
+        },
+        {
+            "name": "No Enchant Cap (Legacy Backport)",
+            "link": "https://modrinth.com/mod/CcwKyFAw",
+            "working": false
+        },
+        {
+            "name": "No Recipe Book (Fabric)",
+            "link": "https://modrinth.com/mod/TvL1V8O5",
+            "working": false
+        }
+    ],
+    "1.7.10": [
+        {
+            "name": "Alternate Current",
+            "link": "https://modrinth.com/mod/r0v8vy1s",
+            "working": false
+        },
+        {
+            "name": "AnTitanic",
+            "link": "https://modrinth.com/mod/GDpQeUxh",
+            "working": false
+        },
+        {
+            "name": "Don't Clear Chat History",
+            "link": "https://modrinth.com/mod/sUbMm93i",
+            "working": false
+        },
+        {
+            "name": "Durability Speed (Legacy Backport)",
+            "link": "https://modrinth.com/mod/6852vWRO",
+            "working": false
+        },
+        {
+            "name": "Fatal Blow - Legacy Backport",
+            "link": "https://modrinth.com/mod/BOhW0XDq",
+            "working": false
+        },
+        {
+            "name": "Infinite Water Bucket",
+            "link": "https://modrinth.com/mod/nNMXZUPi",
+            "working": true
+        },
+        {
+            "name": "Legacy-Elytras",
+            "link": "https://github.com/pixfumy/Legacy-Elytras/releases",
+            "working": true
+        },
+        {
+            "name": "No Enchant Cap (Legacy Backport)",
+            "link": "https://modrinth.com/mod/CcwKyFAw",
+            "working": false
+        },
+        {
+            "name": "OptiFabric",
+            "link": "https://github.com/RedLime/OptiFabric-Pre1.9/releases",
+            "working": true
+        },
+        {
+            "name": "Subtitles Backport",
+            "link": "https://github.com/YetAnotherMcModder/Subtitles-Backport/releases",
+            "working": true
+        }
+    ],
+    "1.6.4": [
+        {
+            "name": "Alternate Current",
+            "link": "https://modrinth.com/mod/r0v8vy1s",
+            "working": false
+        },
+        {
+            "name": "Infinite Water Bucket",
+            "link": "https://modrinth.com/mod/nNMXZUPi",
+            "working": false
+        }
+    ]
+};
+
+let versions = ["1.8.9", "1.13.2", "1.12.2", "1.7.10", "1.6.4"];
+versions.forEach(
+    function (ver) {
+        console.log("loaded " + mods[ver].length + " mods for " + ver);
+    }
+);
