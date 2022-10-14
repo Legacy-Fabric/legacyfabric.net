4d13a845f511d0e06ac026bec3ebe0e298a664db
Howard ZHY <96782166+HowardZHY@users.noreply.github.com>
2022-10-14 12:38:52 +0800

    added TooManyItems

--- res/mods.js
+++ res/mods.js
@@ -277,6 +277,14 @@ const mods = [
         "versions": [ "1.8.9" ]
     },
     {
+        "name": "TooManyItems",
+        "links": {
+            "github": "https://github.com/HowardZHY/TooManyItems-LegacyFabric/releases",
+        },
+        "working": true,
+        "versions": [ "1.8.9", "1.6.4", "1.5.2", "1.4.7", "1.3.2" ]
+    },
+    {
         "name": "Where-Is-My-Stuff",
         "links": {
             "github": "https://github.com/MaheEvil/Where-Is-My-Stuff",
