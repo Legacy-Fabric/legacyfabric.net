f2de7c93c6096f56ac37e0baf63d1abe6cff0a33
tildejustin <spacepepper3.14@gmail.com>
2023-09-01 21:08:38 -0400

    removed my template

--- res/mods.js
+++ res/mods.js
@@ -1,14 +1,3 @@
-// template:
-// {
-//     "name": "",
-//     "links": {
-//         "github": "",
-//         "modrinth": ""
-//     },
-//     "working": true,
-//     "versions": [ "1.13.2", "1.12.2", "1.11.2", "1.10.2", "1.9.4", "1.8.9", "1.7.10", "1.6.4", "1.5.2", "1.4.7", "1.3.2" ]
-// },
-
 const mods = [
     {
         "name": "Alternate Current",
